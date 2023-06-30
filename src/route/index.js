3f22e3acd80e0c79ba949789bddfe81bde1d77ff
Macbook Pro <macbookpro@MacBook-Pro-Macbook.local>
2023-06-30 22:47:44 +0300

    first commit

--- src/route/index.js
+++ src/route/index.js
@@ -8,5 +8,25 @@ router.get('/', function (req, res) {
     text: 'Click me',
   })
 })
-
+router.get('/product', function (req, res) {
+  res.render('product', {
+    product: {
+      title: 'Home page',
+      name: 'Корм для риб' ,
+      price: 72.5 ,
+      img: 'fish.png' ,
+      isAvailable: false,
+    },
+  })
+})
+router.get('/user', function (req, res) {
+  res.render('userinfo', {
+    user: {
+      name: "Іван",
+      email: "ivan@example.com",
+      phone: "123-456-7890" ,
+      balance: 0
+    },
+  })
+})
 module.exports = router
